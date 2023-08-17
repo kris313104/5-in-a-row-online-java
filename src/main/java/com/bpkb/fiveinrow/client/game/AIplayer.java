948bc0a9687e554d67eeb1f757dba3853773e721
bpecko <bartlomiejpecko@gmail.com>
2023-08-17 13:30:21 +0200

    Fixed win logic vs AI, Added simple algorithm to AI to block player winning moves

--- src/main/java/com/bpkb/fiveinrow/client/game/AIplayer.java
+++ src/main/java/com/bpkb/fiveinrow/client/game/AIplayer.java
@@ -8,19 +8,56 @@ import java.util.Random;
 
 public class AIplayer {
 
-    private Random random = new Random();
+    private final Random random = new Random();
 
     public void makeMove(JButton[] buttons) {
+        List<Integer> winningMoves = new ArrayList<>();
+        List<Integer> blockingMoves = new ArrayList<>();
         List<Integer> emptyIndices = new ArrayList<>();
+
         for (int i = 0; i < buttons.length; i++) {
             if (buttons[i].getText().equals("")) {
                 emptyIndices.add(i);
+
+                if (isWinningMove(buttons, i, "O")) {
+                    winningMoves.add(i);
+                } else if (isWinningMove(buttons, i, "X")) {
+                    blockingMoves.add(i);
+                }
             }
         }
-        if (!emptyIndices.isEmpty()) {
+
+        // Choose a move based on strategy priorities
+        if (!winningMoves.isEmpty()) {
+            int winningMove = winningMoves.get(random.nextInt(winningMoves.size()));
+            placeMove(buttons, winningMove, "O");
+        } else if (!blockingMoves.isEmpty()) {
+            int blockingMove = blockingMoves.get(random.nextInt(blockingMoves.size()));
+            placeMove(buttons, blockingMove, "O");
+        } else if (!emptyIndices.isEmpty()) {
             int randomIndex = emptyIndices.get(random.nextInt(emptyIndices.size()));
-            buttons[randomIndex].setForeground(new Color(235, 219, 240));
-            buttons[randomIndex].setText("O");
+            placeMove(buttons, randomIndex, "O");
         }
+    }
+
+    private boolean isWinningMove(JButton[] buttons, int index, String symbol) {
+        buttons[index].setText(symbol);
+        boolean isWinning = WinLogic.checkWin(getCurrentBoardState(buttons), index / 15, index % 15, symbol.charAt(0));
+        buttons[index].setText("");  // Reset the button
+        return isWinning;
+    }
+
+    private void placeMove(JButton[] buttons, int index, String symbol) {
+        buttons[index].setForeground(new Color(235, 219, 240));
+        buttons[index].setText(symbol);
+    }
+
+    private char[][] getCurrentBoardState(JButton[] buttons) {
+        char[][] board = new char[15][15];
+        for (int i = 0; i < buttons.length; i++) {
+            char symbol = buttons[i].getText().isEmpty() ? ' ' : buttons[i].getText().charAt(0);
+            board[i / 15][i % 15] = symbol;
+        }
+        return board;
     }
 }
